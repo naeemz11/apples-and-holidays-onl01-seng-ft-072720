32472b22ac7d490b9b3a06df6a3e20571220b536
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 18:23:01 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -47,7 +47,7 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   }
   
   holiday_hash[:winter] = {
-    valentines_day: [""]
+    valentines_day: ["Cupid Cut-Out", "Candy Hearts"]
   }
   
 holiday_hash
