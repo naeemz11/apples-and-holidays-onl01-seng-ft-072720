7618569a30c8f6b71c462836018c19c79c746cce
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 18:07:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,7 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_hash[:winter].each do |holidays, decorations|
-    decorations << suppy
+    decorations << supply
 
 end
 
