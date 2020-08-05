9fde05794ccc876dfef0a07d40fee4812cad2967
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 19:05:51 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -52,7 +52,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-holiday_hash[:winter].collect do |holidays, supplies|
+array= holiday_hash[:winter].collect do |holidays, supplies|
   supplies
 end
 holiday_hash.flatten
