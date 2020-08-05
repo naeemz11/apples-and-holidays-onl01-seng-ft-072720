95becafdb714ae629da8987ffd3254bc785c5b88
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 18:41:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,7 @@ def all_supplies_in_holidays(holiday_hash)
 holiday_hash.each do |season, holiday|
   puts "#{season.capitalize}:"
   holiday.each do |holiday, supply|
-    puts "#{holiday.to_s"
+    puts "#{holiday.to_s.splt"
 
 end
 
