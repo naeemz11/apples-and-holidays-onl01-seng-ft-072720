aa3542e3ef7951b08815defc448bdbf713bda584
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-05 18:38:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,7 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-holiday_hash.each do |season, holiday
+holiday_hash.each do |season, holiday|
+  puts "#{season.capitalize}"
 
 end
 
